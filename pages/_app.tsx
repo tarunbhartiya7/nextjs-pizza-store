629ee34d032a2db5d34d4f54e6690f3ccda2bb8a
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2021-08-02 19:08:02 +0530

    add material ui and icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log('testing')
   return <Component {...pageProps} />
 }
 export default MyApp
