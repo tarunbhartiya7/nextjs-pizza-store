421a402ef126611ce6c30863ddf0ce6c6d71af2f
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2021-08-02 22:07:46 +0530

    add big image

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -10,7 +10,7 @@ export default function FirstPost() {
         <Head>
           <title>First Post</title>
         </Head>
-        <h1>First Post</h1>
+        <h1>Cloudinary image</h1>
         <Image
           src="https://res.cloudinary.com/dexkvngbv/image/upload/v1627920277/dubai_aqua_park_vamb1o.jpg" // Route of the image file
           height={150} // Desired size with correct aspect ratio
@@ -18,11 +18,19 @@ export default function FirstPost() {
           alt="Your Name"
           priority
         />
-        <h1>Second Post</h1>
+        <h1>Local static jpeg</h1>
         <Image
           src="/images/profile.jpeg" // Route of the image file
           height={150} // Desired size with correct aspect ratio
           width={150} // Desired size with correct aspect ratio
+          alt="Your Name"
+          priority
+        />
+        <h1>local png image</h1>
+        <Image
+          src="/images/test_image.png" // Route of the image file
+          height={810} // Desired size with correct aspect ratio
+          width={1080} // Desired size with correct aspect ratio
           alt="Your Name"
           priority
         />
