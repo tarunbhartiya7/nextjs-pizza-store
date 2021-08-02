1de940fbd0669a1e74f77f748b88dc5eab99ebbf
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2021-08-02 21:43:41 +0530

    add cloudinary

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -12,6 +12,14 @@ export default function FirstPost() {
         </Head>
         <h1>First Post</h1>
         <Image
+          src="https://res.cloudinary.com/dexkvngbv/image/upload/v1627920277/dubai_aqua_park_vamb1o.jpg" // Route of the image file
+          height={150} // Desired size with correct aspect ratio
+          width={150} // Desired size with correct aspect ratio
+          alt="Your Name"
+          priority
+        />
+        <h1>Second Post</h1>
+        <Image
           src="/images/profile.jpeg" // Route of the image file
           height={150} // Desired size with correct aspect ratio
           width={150} // Desired size with correct aspect ratio
